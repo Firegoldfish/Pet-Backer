19873c458c93b7a73e184ea4f9e67d9d9297a22a
Bill Madison <Firegoldfish@outlook.com>
2025-03-09 23:43:20 +0800

    initial

---
+++ pet-system/src/main/java/com/hjy/PetSystyemApplication.java
@@ -0,0 +1,13 @@
+package com.hjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSystyemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetSystyemApplication.class, args);
+    }
+
+}
