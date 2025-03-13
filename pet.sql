e9b4bc19a8ed3dcf69a35b18b7afcee55c6d9506
bill <firegoldfish@outlook.com>
2025-03-13 16:17:20 +0800

    优化设计数据库

--- pet.sql
+++ pet.sql
@@ -1,5 +1,3 @@
- 
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -8,18 +6,18 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `pet_admin`;
 CREATE TABLE `pet_admin`  (
-  `ADMIN_ID` int NOT NULL AUTO_INCREMENT COMMENT '管理员ID',
-  `ADMIN_NAME` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '管理员名称',
-  `ADMIN_PASSWORD` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '管理员密码',
-  `ADMIN_DESC` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `ADMIN_PHONE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `ADMIN_LOGIN_CNT` int NOT NULL DEFAULT 0,
-  `ADMIN_TYPE` int NOT NULL DEFAULT 0,
-  `ADMIN_STATUS` int NOT NULL DEFAULT 1,
-  `ADMIN_LOGIN_TIME` bigint NOT NULL DEFAULT 0,
-  `ADD_TIME` bigint NOT NULL DEFAULT 0,
-  `EDIT_TIME` bigint NOT NULL DEFAULT 0,
-  PRIMARY KEY (`ADMIN_ID`) USING BTREE
+                              `admin_id` int NOT NULL AUTO_INCREMENT COMMENT '管理员ID',
+                              `admin_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '管理员名称',
+                              `admin_password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '管理员密码',
+                              `admin_desc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '管理员描述',
+                              `admin_phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '管理员电话',
+                              `admin_login_cnt` int NOT NULL DEFAULT 0 COMMENT '管理员登录次数',
+                              `admin_type` int NOT NULL DEFAULT 0 COMMENT '管理员类型（0-普通管理员，1-超级管理员）',
+                              `admin_status` int NOT NULL DEFAULT 1 COMMENT '管理员状态（0-禁用，1-启用）',
+                              `admin_login_time` bigint NOT NULL DEFAULT 0 COMMENT '管理员最近登录时间',
+                              `add_time` bigint NOT NULL DEFAULT 0 COMMENT '添加时间',
+                              `edit_time` bigint NOT NULL DEFAULT 0 COMMENT '编辑时间',
+                              PRIMARY KEY (`admin_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 INSERT INTO `pet_admin` VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'admin', NULL, 1, 1, 1, 1735302901541, 0, 1734786380484);
@@ -29,15 +27,15 @@ INSERT INTO `pet_admin` VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 
 -- ----------------------------
 DROP TABLE IF EXISTS `pet_fav`;
 CREATE TABLE `pet_fav`  (
-  `FAV_ID` int NOT NULL AUTO_INCREMENT,
-  `FAV_USER_ID` int NOT NULL DEFAULT 0,
-  `FAV_TITLE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `FAV_TYPE` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `FAV_OID` int NOT NULL DEFAULT 0,
-  `FAV_PATH` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `ADD_TIME` bigint NOT NULL DEFAULT 0,
-  `EDIT_TIME` bigint NOT NULL DEFAULT 0,
-  PRIMARY KEY (`FAV_ID`) USING BTREE
+                            `fav_id` int NOT NULL AUTO_INCREMENT COMMENT '收藏ID',
+                            `fav_user_id` int NOT NULL DEFAULT 0 COMMENT '用户ID',
+                            `fav_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '收藏标题',
+                            `fav_type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '收藏类型（如服务、商品等）',
+                            `fav_oid` int NOT NULL DEFAULT 0 COMMENT '收藏对象ID',
+                            `fav_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '收藏对象路径',
+                            `add_time` bigint NOT NULL DEFAULT 0 COMMENT '添加时间',
+                            `edit_time` bigint NOT NULL DEFAULT 0 COMMENT '编辑时间',
+                            PRIMARY KEY (`fav_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -45,21 +43,21 @@ CREATE TABLE `pet_fav`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `pet_meet`;
 CREATE TABLE `pet_meet`  (
-  `MEET_ID` int NOT NULL AUTO_INCREMENT,
-  `MEET_TITLE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `MEET_CATE_ID` int NOT NULL DEFAULT 0,
-  `MEET_CATE_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `MEET_STATUS` int NOT NULL DEFAULT 1,
-  `MEET_ORDER` int NOT NULL DEFAULT 9999,
-  `MEET_VOUCH` int NOT NULL DEFAULT 0,
-  `MEET_DAYS` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `MEET_VIEW_CNT` int NOT NULL DEFAULT 0,
-  `MEET_MAX_CNT` int NOT NULL DEFAULT 0,
-  `MEET_FORMS` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `MEET_OBJ` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `ADD_TIME` bigint NOT NULL DEFAULT 0,
-  `EDIT_TIME` bigint NOT NULL DEFAULT 0,
-  PRIMARY KEY (`MEET_ID`) USING BTREE
+                             `meet_id` int NOT NULL AUTO_INCREMENT COMMENT '服务预约ID',
+                             `meet_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '服务标题',
+                             `meet_cate_id` int NOT NULL DEFAULT 0 COMMENT '服务分类ID',
+                             `meet_cate_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '服务分类名称',
+                             `meet_status` int NOT NULL DEFAULT 1 COMMENT '服务状态（0-关闭，1-开启）',
+                             `meet_order` int NOT NULL DEFAULT 9999 COMMENT '服务排序',
+                             `meet_vouch` int NOT NULL DEFAULT 0 COMMENT '服务推荐标识（0-不推荐，1-推荐）',
+                             `meet_days` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '服务可用日期',
+                             `meet_view_cnt` int NOT NULL DEFAULT 0 COMMENT '服务浏览次数',
+                             `meet_max_cnt` int NOT NULL DEFAULT 0 COMMENT '服务最大预约次数',
+                             `meet_forms` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '服务表单配置',
+                             `meet_obj` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '服务扩展信息',
+                             `add_time` bigint NOT NULL DEFAULT 0 COMMENT '添加时间',
+                             `edit_time` bigint NOT NULL DEFAULT 0 COMMENT '编辑时间',
+                             PRIMARY KEY (`meet_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 37 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -67,21 +65,21 @@ CREATE TABLE `pet_meet`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `pet_meet_join`;
 CREATE TABLE `pet_meet_join`  (
-  `MEET_JOIN_ID` int NOT NULL AUTO_INCREMENT,
-  `MEET_JOIN_USER_ID` int NOT NULL DEFAULT 0,
-  `MEET_JOIN_MEET_ID` int NOT NULL DEFAULT 0,
-  `MEET_JOIN_CODE` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `MEET_JOIN_IS_CHECK` int NOT NULL DEFAULT 0,
-  `MEET_JOIN_CHECK_TIME` bigint NOT NULL DEFAULT 0,
-  `MEET_JOIN_FORMS` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `MEET_JOIN_OBJ` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `MEET_JOIN_STATUS` int NOT NULL DEFAULT 0,
-  `MEET_JOIN_TIME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `MEET_JOIN_DAY` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `MEET_JOIN_MEET_TITLE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `ADD_TIME` bigint NOT NULL DEFAULT 0,
-  `EDIT_TIME` bigint NOT NULL DEFAULT 0,
-  PRIMARY KEY (`MEET_JOIN_ID`) USING BTREE
+                                  `meet_join_id` int NOT NULL AUTO_INCREMENT COMMENT '服务预约参与ID',
+                                  `meet_join_user_id` int NOT NULL DEFAULT 0 COMMENT '用户ID',
+                                  `meet_join_meet_id` int NOT NULL DEFAULT 0 COMMENT '服务预约ID',
+                                  `meet_join_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '预约码',
+                                  `meet_join_is_check` int NOT NULL DEFAULT 0 COMMENT '是否已核验（0-未核验，1-已核验）',
+                                  `meet_join_check_time` bigint NOT NULL DEFAULT 0 COMMENT '核验时间',
+                                  `meet_join_forms` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '预约表单数据',
+                                  `meet_join_obj` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '预约扩展信息',
+                                  `meet_join_status` int NOT NULL DEFAULT 0 COMMENT '预约状态（0-待确认，1-已确认，2-已取消）',
+                                  `meet_join_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '预约时间',
+                                  `meet_join_day` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '预约日期',
+                                  `meet_join_meet_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '服务标题',
+                                  `add_time` bigint NOT NULL DEFAULT 0 COMMENT '添加时间',
+                                  `edit_time` bigint NOT NULL DEFAULT 0 COMMENT '编辑时间',
+                                  PRIMARY KEY (`meet_join_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 11193 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -89,21 +87,21 @@ CREATE TABLE `pet_meet_join`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `pet_news`;
 CREATE TABLE `pet_news`  (
-  `NEWS_ID` int NOT NULL AUTO_INCREMENT,
-  `NEWS_TITLE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `NEWS_CATE_ID` int NOT NULL DEFAULT 0,
-  `NEWS_CATE_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `NEWS_STATUS` int NOT NULL DEFAULT 1,
-  `NEWS_ORDER` int NOT NULL DEFAULT 9999,
-  `NEWS_VOUCH` int NOT NULL DEFAULT 0,
-  `NEWS_CONTENT` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
-  `NEWS_VIEW_CNT` int NOT NULL DEFAULT 0,
-  `NEWS_PIC` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `NEWS_OBJ` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
-  `NEWS_FORMS` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
-  `ADD_TIME` bigint NOT NULL DEFAULT 0,
-  `EDIT_TIME` bigint NOT NULL DEFAULT 0,
-  PRIMARY KEY (`NEWS_ID`) USING BTREE
+                             `news_id` int NOT NULL AUTO_INCREMENT COMMENT '新闻ID',
+                             `news_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '新闻标题',
+                             `news_cate_id` int NOT NULL DEFAULT 0 COMMENT '新闻分类ID',
+                             `news_cate_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '新闻分类名称',
+                             `news_status` int NOT NULL DEFAULT 1 COMMENT '新闻状态（0-禁用，1-启用）',
+                             `news_order` int NOT NULL DEFAULT 9999 COMMENT '新闻排序',
+                             `news_vouch` int NOT NULL DEFAULT 0 COMMENT '新闻推荐标识（0-不推荐，1-推荐）',
+                             `news_content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '新闻内容',
+                             `news_view_cnt` int NOT NULL DEFAULT 0 COMMENT '新闻浏览次数',
+                             `news_pic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '新闻图片',
+                             `news_obj` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '新闻扩展信息',
+                             `news_forms` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '新闻表单配置',
+                             `add_time` bigint NOT NULL DEFAULT 0 COMMENT '添加时间',
+                             `edit_time` bigint NOT NULL DEFAULT 0 COMMENT '编辑时间',
+                             PRIMARY KEY (`news_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 66 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -111,13 +109,13 @@ CREATE TABLE `pet_news`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `pet_setup`;
 CREATE TABLE `pet_setup`  (
-  `SETUP_ID` int NOT NULL AUTO_INCREMENT,
-  `SETUP_TYPE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `SETUP_KEY` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `SETUP_VALUE` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `ADD_TIME` bigint NOT NULL DEFAULT 0,
-  `EDIT_TIME` bigint NOT NULL DEFAULT 0,
-  PRIMARY KEY (`SETUP_ID`) USING BTREE
+                              `setup_id` int NOT NULL AUTO_INCREMENT COMMENT '系统配置ID',
+                              `setup_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '配置类型',
+                              `setup_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '配置键',
+                              `setup_value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '配置值',
+                              `add_time` bigint NOT NULL DEFAULT 0 COMMENT '添加时间',
+                              `edit_time` bigint NOT NULL DEFAULT 0 COMMENT '编辑时间',
+                              PRIMARY KEY (`setup_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -125,18 +123,18 @@ CREATE TABLE `pet_setup`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `pet_user`;
 CREATE TABLE `pet_user`  (
-  `USER_ID` int NOT NULL AUTO_INCREMENT,
-  `USER_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `USER_ACCOUNT` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `USER_STATUS` int NOT NULL DEFAULT 1,
-  `USER_PASSWORD` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `USER_LOGIN_TIME` bigint NOT NULL DEFAULT 0,
-  `USER_LOGIN_CNT` int NOT NULL DEFAULT 0,
-  `USER_OBJ` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
-  `USER_FORMS` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
-  `ADD_TIME` bigint NOT NULL DEFAULT 0,
-  `EDIT_TIME` bigint NOT NULL DEFAULT 0,
-  PRIMARY KEY (`USER_ID`) USING BTREE
+                             `user_id` int NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+                             `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名称',
+                             `user_account` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户账号',
+                             `user_status` int NOT NULL DEFAULT 1 COMMENT '用户状态（0-禁用，1-启用）',
+                             `user_password` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户密码',
+                             `user_login_time` bigint NOT NULL DEFAULT 0 COMMENT '用户最近登录时间',
+                             `user_login_cnt` int NOT NULL DEFAULT 0 COMMENT '用户登录次数',
+                             `user_obj` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '用户扩展信息',
+                             `user_forms` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '用户表单配置',
+                             `add_time` bigint NOT NULL DEFAULT 0 COMMENT '添加时间',
+                             `edit_time` bigint NOT NULL DEFAULT 0 COMMENT '编辑时间',
+                             PRIMARY KEY (`user_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 116 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
