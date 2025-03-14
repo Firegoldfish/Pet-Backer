9c4d29fcda3c8594ab912e3af9c8982ab3c58f57
Bill Madison <Firegoldfish@outlook.com>
2025-03-14 16:52:38 +0800

    项目结构

--- pet-server/src/main/java/com/hjy/PetApplication.java
+++ pet-server/src/main/java/com/hjy/PetApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetSystyemApplication {
+public class PetApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetSystyemApplication.class, args);
+        SpringApplication.run(PetApplication.class, args);
     }
 
 }
