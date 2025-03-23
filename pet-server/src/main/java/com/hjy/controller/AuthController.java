a6c12e2d7d99558fdf11145181182a52f1dd7e29
bill <firegoldfish@outlook.com>
2025-03-23 10:48:52 +0800

    权限管理基本框架

---
+++ pet-server/src/main/java/com/hjy/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.hjy.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+@Slf4j
+@Api(tags = "权限管理")
+public class AuthController {
+
+}
---
+++ pet-server/src/main/java/com/hjy/service/impl/AuthServiceImpl.java
@@ -0,0 +1,10 @@
+package com.hjy.service.impl;
+
+import com.hjy.service.AuthService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AuthServiceImpl implements AuthService {
+}
